f3d7ad51f83e54c2716c1e6e7e37202645d72846
Anan Wolf <ananwolf1@gmail.com>
2021-05-10 11:41:53 -0700

    updating groceryList

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // TODO
+var GroceryList = (prop) => (
+  <ul>
+    {prop.groceries.map((item, i) =>
+      <GroceryList key={i} item={item}/>
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceries={['Eggs', 'Veggies']}/>
+  </div>
+);
